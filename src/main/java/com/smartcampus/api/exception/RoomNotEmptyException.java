6afacf9144ce92b64f3f8c66a7c3c6fca988201a
Nipun Nirman <164348907+nipunnirman@users.noreply.github.com>
2026-04-21 19:22:57 +0530

    remove some codefile in level 3 implementation

--- src/main/java/com/smartcampus/api/exception/RoomNotEmptyException.java
+++
@@ -1,7 +0,0 @@
-package com.smartcampus.api.exception;
-
-public class RoomNotEmptyException extends RuntimeException {
-    public RoomNotEmptyException(String message) {
-        super(message);
-    }
-}
